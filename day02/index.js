5d43b9acd8f57503c5bceb1e5df1018a7326a361
Claire Froelich <claire.froelich@gmail.com>
2021-12-01 22:04:28 -0800

    Clean code

--- day02/index.js
+++ day02/index.js
@@ -27,7 +27,6 @@ class Tracker {
   };
 
   track([op, n]) {
-    // console.log({ op, n });
     const fn = this.ops[op];
     if (!fn) {
       console.warn(`No op found for '${op}'`);
@@ -66,7 +65,6 @@ class AimTracker {
   };
 
   track([op, n]) {
-    // console.log({ op, n });
     const fn = this.ops[op];
     if (!fn) {
       console.warn(`No op found for '${op}'`);
